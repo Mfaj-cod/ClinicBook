950d32911240a85a51b89184108651149de06ad0
M. fajal <fajalmoaj@gmail.com>
2025-11-28 17:46:01 +0530

    model knows appointments

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,20 @@
 from .logg import logger
 from .init_db import setup
 from .seed import seed
+from .gem import get_db_connection, run_query, save_chat_log, get_chat_history_for_gemini, search_doctor_by_specialization, search_appointments_by_patient, search_clinic_by_city, search_doctor_by_name, extract_text, gemini_chat
 
 __all__ = [
     'setup',
     'seed',
     'logger',
+    'get_db_connection',
+    'run_query',
+    'save_chat_log',
+    'get_chat_history_for_gemini',
+    'search_doctor_by_specialization',
+    'search_doctor_by_name',
+    'search_clinic_by_city',
+    'search_appointments_by_patient',
+    'extract_text',
+    'gemini_chat',
 ]
