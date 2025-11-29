269b008079b0ceb127ee7d0cf407347f2e8f2ffa
M. fajal <fajalmoaj@gmail.com>
2025-11-29 22:20:38 +0530

    AI assistant can listen you now along with Lightweight RAG integration

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from .logg import logger
 from .init_db import setup
 from .seed import seed
-from .gem import get_db_connection, run_query, save_chat_log, get_chat_history_for_gemini, search_doctor_by_specialization, search_appointments_by_patient, search_clinic_by_city, search_doctor_by_name, gemini_chat, cancel_appointment_by_patient, get_response_text, complete_appointment_by_doctor, clean_text_for_display
+from .gem import get_db_connection, run_query, save_chat_log, get_chat_history_for_gemini, search_doctor_by_specialization, search_appointments_by_patient, search_clinic_by_city, search_doctor_by_name, gemini_chat, cancel_appointment_by_patient, get_response_text, complete_appointment_by_doctor, clean_text_for_display, generate_slots_by_doctor, get_doctor_schedule, book_appointment_by_patient, get_available_slots
 
 __all__ = [
     'setup',
@@ -19,5 +19,9 @@ __all__ = [
     'cancel_appointment_by_patient',
     'clean_text_for_display',
     'complete_appointment_by_doctor',
+    'generate_slots_by_doctor',
+    'get_doctor_schedule',
+    'book_appointment_by_patient',
+    'get_available_slots',
     'gemini_chat',
 ]
