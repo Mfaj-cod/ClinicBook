fdf28fe6c620d8300a0feee46aa55babcaa6a72c
M. fajal <fajalmoaj@gmail.com>
2025-12-08 11:11:43 +0530

    log dir

--- src/logg.py
+++ src/logg.py
@@ -1,4 +1,9 @@
 import logging
+import os
+
+LOG_DIR = './logs'
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -7,7 +12,7 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # file handler
-fh = logging.FileHandler('./logs/app.log')
+fh = logging.FileHandler(os.path.join(LOG_DIR, 'app.log'))
 fh.setLevel(logging.INFO)
 fh.setFormatter(formatter)
 
